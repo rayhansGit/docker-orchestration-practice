cf0b540aa47c6f247a89df0d6142d65feacc2a3c
showrov016 <showrov.rayhan@gmail.com>
2022-10-12 18:23:24 +0300

    containers communication fixed

--- server1/server.js
+++ server1/server.js
@@ -10,6 +10,8 @@ const app = express();
 app.get('/', (req, res) => {
   console.log("Req came from " + req.client.remoteAddress + ":" + req.client.remotePort);
   console.log("Req served at " + req.client.localAddress + ":" + req.client.localPort);
+
+  fetch("http://docker-orchestration-practice-server2-1:3000");
 });
 
 app.listen(PORT);

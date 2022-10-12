f8b3b97b7ddf2e86da31948a757d41493029320c
WKS-97999-LT\tunii <md.islam@tuni.fi>
2022-10-12 16:28:49 +0300

    initial commit

---
+++ server2/server.js
@@ -0,0 +1,14 @@
+'use strict';
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  console.log("Req came from " + req.client.remoteAddress + ":" + req.client.remotePort);
+  console.log("Req served at " + req.client.localAddress + ":" + req.client.localPort);
+});
+
+app.listen(PORT);
